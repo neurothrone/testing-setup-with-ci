c511aed86772d853e511b64299fc0b95fe4865ae
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-02-17 23:02:37 +0100

    add multiply and divide

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,9 +1,21 @@
-import calclator
+import pytest
+
+import calculator
 
 
 class TestCalculator:
     def test_addition(self):
-        assert calclator.add(2, 2) == 4
+        assert calculator.add(2, 2) == 4
 
     def test_subtraction(self):
-        assert calclator.subtract(3, 1) == 2
+        assert calculator.subtract(3, 1) == 2
+
+    def test_division(self):
+        assert calculator.divide(5, 5) == 1.0
+
+    def test_division_division_by_zero(self):
+        with pytest.raises(ZeroDivisionError):
+            calculator.divide(5, 0)
+
+    def test_multiplication(self):
+        assert calculator.multiply(3, 5) == 15
